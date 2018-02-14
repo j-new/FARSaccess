bcc7b8fc9df68b62826267c4e72aa9e556488543
New, Jeremy <jnew@royall.com>
2018-02-14 15:31:12 -0500

    fix tests

--- tests/testthat/tests_all.R
+++ tests/testthat/tests_all.R
@@ -4,10 +4,10 @@ test_that("Filename generator produces path with generally correct formatting", 
 })
 
 
-# test_that("Data files have expected naming and path", {
-#   expect_true(file.exists(file.path(system.file("data", package="FARSaccess"), "accident_2013.csv.bz2")))
-#   expect_true(file.exists(file.path(system.file("data", package="FARSaccess"), "accident_2014.csv.bz2")))
-#   expect_true(file.exists(file.path(system.file("data", package="FARSaccess"), "accident_2015.csv.bz2")))
-# })
+test_that("Data files have expected naming and path", {
+  expect_true(file.exists(file.path(system.file("data", package="FARSaccess"), "accident_2013.csv.bz2")))
+  expect_true(file.exists(file.path(system.file("data", package="FARSaccess"), "accident_2014.csv.bz2")))
+  expect_true(file.exists(file.path(system.file("data", package="FARSaccess"), "accident_2015.csv.bz2")))
+})
 
 
